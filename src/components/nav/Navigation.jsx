4b5a6b0f9e1d01db292d9de820fda20384b7a358
Masa Orel <orel.masha89@gmail.com>
2023-04-23 15:16:49 +0200

    added shopping cart icon

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./navigation.css";
 import { Link } from "react-router-dom";
 
 function Navigation() {
+  const [items, setItems] = useState(0);
+
   return (
     <header>
       <nav>
@@ -16,6 +19,10 @@ function Navigation() {
           <li>
             <Link to="#">Contact</Link>
           </li>
+          <button className="cart">
+            <i className="fa-solid fa-cart-shopping"></i>
+            <span className="items-number">{items}</span>
+          </button>
         </ul>
       </nav>
     </header>
