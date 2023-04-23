5a5f7a9ec967bbe1e7edbd922e9bde1af4cbe243
Masa Orel <orel.masha89@gmail.com>
2023-04-23 10:40:16 +0200

    added navigation

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -1,7 +1,25 @@
-import "./navigation.css"
+import "./navigation.css";
+import { Link } from "react-router-dom";
 
 function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <header>
+      <nav>
+        <h1>logo</h1>
+        <ul className="nav-items">
+          <li>
+            <Link to="#">Home</Link>
+          </li>
+          <li>
+            <Link to="#">Shop</Link>
+          </li>
+          <li>
+            <Link to="#">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Navigation;
