029d80eb6c5394bb593c7b8f66bb5a2ace78f4fc
Masa Orel <orel.masha89@gmail.com>
2023-04-23 10:31:38 +0200

    basic starting structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Navigation from "./components/nav/Navigation";
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Navigation /> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,7 @@
+import "./hero.css"
+
+function Hero() {
+  return <div>Hero</div>;
+}
+
+export default Hero;
