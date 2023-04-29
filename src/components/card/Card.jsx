7c24bfa41060004177720a0ec2bc60e96fbc9362
Masa Orel <orel.masha89@gmail.com>
2023-04-29 15:30:24 +0200

    added zoom effect on card img hover

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,15 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import formatCurrency from "../../utils/formatCurrency";
 import "./card.css";
 
 function Card({ image, name, price, id }) {
-
-
   return (
     <div className="card">
-      <Link to={`/shopping-cart/products/${id}`}>
-      <img src={image} alt="" />
-      </Link>
+      <div className="card-image">
+        <Link to={`/shopping-cart/products/${id}`}>
+          <img src={image} alt="" />
+        </Link>
+      </div>
       <div className="details">
         <h3 className="name">{name}</h3>
         <h4 className="price">{formatCurrency(price)}</h4>
