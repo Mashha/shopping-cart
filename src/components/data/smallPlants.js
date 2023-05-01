7361cb6aec492e634dcff1c9d6793690dc2da95d
Masa Orel <orel.masha89@gmail.com>
2023-05-01 22:10:55 +0200

    updated small plants object and added more images

--- src/components/data/smallPlants.js
+++ src/components/data/smallPlants.js
@@ -1,8 +1,23 @@
-import money_tree from "../../images/plants/Money_Tree.png";
-import money_tree_close from "../../images/plants/Money_Tree_Close.jpg"
-import philodendron from "../../images/plants/Philodendron_Birkin.png";
-import rattlesnake from "../../images/plants/Calathea_Rattlesnake.png";
-import zzPlant from "../../images/plants/ZZ_plant.png";
+import money_tree from "../../images/plants/money_tree.jpg";
+import money_tree_close from "../../images/plants/money_tree_close.jpg";
+import philodendron from "../../images/plants/philodendron.jpg";
+import philodendron_close from "../../images/plants/philodendron-close.jpg";
+import calathea from "../../images/plants/calathea.jpg";
+import calathea_close from "../../images/plants/calathea-close.jpg";
+import zzPlant from "../../images/plants/zz-plant.jpg";
+import zz_close from "../../images/plants/zz-plant-close.jpeg";
+import rubber_tree from "../../images/plants/rubber-tree.jpg";
+import rubber_tree_close from "../../images/plants/rubber-tree-close.jpg";
+import ponytail from "../../images/plants/ponytail-palm.jpg";
+import ponytail_close from "../../images/plants/ponytail-palm-close.jpg";
+import spider_plant from "../../images/plants/spider-plant.jpg";
+import spider_plant_close from "../../images/plants/spider-plant-close.jpg";
+import zebrina from "../../images/plants/zebrina.jpg";
+import zebrina_close from "../../images/plants/zebrina_close.jpg";
+import parlor from "../../images/plants/parlor-palm.jpg";
+import parlor_palm_close from "../../images/plants/parlor-close.jpg";
+import snake_plant from "../../images/plants/snake_plant.jpg";
+import snake_plant_close from "../../images/plants/snake_plant_close.jpg";
 
 export const smallPlants = [
   {
@@ -11,54 +26,118 @@ export const smallPlants = [
     image: money_tree,
     price: 68,
     description:
-      "The Money Tree is the perfect low-care indoor plant to bring a tropical feel with a touch of good fortune. With its intricately braided trunk and bright green, palm-like leaves, it looks both like a tree and a palm. The Money Tree gets its name in part from the natural Feng Shui this plant gives. It's believed this plant will bring positive energy and good luck to the owner. The gorgeous Money Tree makes for a meaningful gift or your own good luck charm.",
+      "Said to bring good luck and fortune, the Money Tree is the perfect plant to add to any room of your home to create good Feng Shui. It is known for its resilience, ease of growth, and fun braided trunk.",
     size: "small",
-    gallery: [
-      money_tree,
-      money_tree_close
-    ],
-    qty: 1
+    gallery: [money_tree, money_tree_close],
+    qty: 1,
+    petFriendly: true,
   },
   {
-    name: "Philodendron Birkin",
-    id: "philodendron_birkin",
+    name: "Philodendron Green",
+    id: "philodendron_green",
     image: philodendron,
-    price: 48,
+    price: 49,
     description:
-      "A lively, pet-friendly plant topped with palmate leaves upon a stout trunk. A mini-version of the beloved braided Money Tree",
-      gallery: [
-        money_tree,
-        philodendron,
-        zzPlant
-      ],
-      qty: 1
+      "Why is the Philodendron our most popular plant year after year? It could be its heart-shaped green leaves, easy-going nature, or quick-growing trailing vines. Snag this low-maintenance houseplant now to bring the outdoors in.",
+    size: "small",
+    gallery: [philodendron, philodendron_close],
+    qty: 1,
+    petFriendly: false,
   },
   {
-    name: "Calathea Rattlesnake",
-    id: "calathea_rattlesnake",
-    image: rattlesnake,
-    price: 58,
+    name: "Calathea Freddie",
+    id: "calathea_freddie",
+    image: calathea,
+    price: 49,
     description:
-      "A lively, pet-friendly plant topped with palmate leaves upon a stout trunk. A mini-version of the beloved braided Money Tree",
-      gallery: [
-        money_tree,
-        philodendron,
-        zzPlant
-      ],
-      qty: 1
+      "Calathea Freddie is a smaller variety of Calathea with leaves that grow in a rosette out from the center of the plant. The delicate, wavy feather-shaped leaves are light green with dark green striped markings. During the day, the leaves open up outwards from the center and in the evening, close up like hands in prayer, giving it the nickname, “Prayer Plant.",
+    size: "small",
+    gallery: [calathea, calathea_close],
+    qty: 1,
+    petFriendly: true,
   },
   {
     name: "ZZ Plant",
     id: "zz-plant",
     image: zzPlant,
-    price: 48,
+    price: 59,
     description:
-      "A lively, pet-friendly plant topped with palmate leaves upon a stout trunk. A mini-version of the beloved braided Money Tree",
-      gallery: [
-        money_tree,
-        philodendron,
-        zzPlant
-      ],
-      qyt: 1
+      "The ZZ Plant is characterized by its waxy green leaves above the surface of its potting mix, and its large potato-like rhizomes underneath. These rhizomes store water, making the ZZ a hardy, drought-tolerant houseplant that only needs water every few weeks.",
+    size: "small",
+    gallery: [zzPlant, zz_close],
+    qyt: 1,
+    petFriendly: false,
+  },
+  {
+    name: "Rubber Tree",
+    id: "rubber-tree",
+    image: rubber_tree,
+    price: 99,
+    description:
+      "The Ficus Elastica, or rubber tree, is known for its thick, glossy leaves and latex sap that was once used in rubber production! This same sap makes the Ficus elastica toxic, so the best practice is to keep it out of the reach of curious pets and small children. Give your Rubber tree plenty of bright indirect light to help it retain its dramatic foliage.",
+    size: "small",
+    gallery: [rubber_tree, rubber_tree_close],
+    qyt: 1,
+    petFriendly: false,
+  },
+  {
+    name: "Parlor Palm",
+    id: "parlor-palm",
+    image: parlor,
+    price: 69,
+    description:
+      "The Parlor Palm is a favorite easy-care palm with tropical fronds known for its air-purifying qualities.",
+    size: "small",
+    gallery: [parlor, parlor_palm_close],
+    qyt: 1,
+    petFriendly: true,
+  },
+  {
+    name: "Spider Plant",
+    id: "spider-plant",
+    image: spider_plant,
+    price: 39,
+    description:
+      "The Spider Plant's easy-going nature and green and white striped foliage make it one of the most popular houseplants around. It gets its name from the baby plants, or spiderettes, that dangle down from a mature mother plant like spiders on a web. Despite its creepy-crawly name, the Spider Plant is a lush, easy-care pick perfect for bringing life to your space.",
+    size: "small",
+    gallery: [spider_plant, spider_plant_close],
+    qyt: 1,
+    petFriendly: true,
+  },
+  {
+    name: "Ponytail Palm",
+    id: "ponytail-palm",
+    image: ponytail,
+    price: 79,
+    description:
+      "The Ponytail Palm is drought tolerant, slow-growing, and requires very little care. This plant is ideal for people with very little time or who travel regularly. The Ponytail Palm will be perfectly happy being watered every couple of weeks and left alone to soak up the sunlight. This distinct plant brings a little fun to any room in your home.",
+    size: "small",
+    gallery: [ponytail, ponytail_close],
+    qyt: 1,
+    petFriendly: true,
+  },
+  {
+    name: "Snake Plant",
+    id: "snake-plant",
+    image: snake_plant,
+    price: 39,
+    description:
+      "The Snake Plant Laurentii, or Sansevieria trifasciata 'Laurentii', is a succulent plant characterized by its upright sword-like leaves with vibrant yellow edges. It is popular for its incredibly easy-going nature (it can tolerate low light and drought) and its air-purifying capabilities. The easiest way to kill this plant is to overcare for it.",
+    size: "small",
+    gallery: [snake_plant, snake_plant_close],
+    qyt: 1,
+    petFriendly: false,
+  },
+  {
+    name: "Tradescantia Zebrina",
+    id: "zebrina",
+    image: zebrina,
+    price: 49,
+    description:
+      "Tradescantia Zebrina is a fast-growing trailing plant that gets its name from the colorful zebra-striped leaves. The vibrant colors on this Tradescantia make it a perfect accent to any collection looking for a pop of color.",
+    size: "small",
+    gallery: [zebrina, zebrina_close],
+    qyt: 1,
+    petFriendly: false,
   },
 ];
