f2b92d69a03155f30c3d69cd9e017b31f83af86a
Masa Orel <orel.masha89@gmail.com>
2023-05-05 15:15:23 +0200

    added box shadow, padding on hero, changed image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -23,37 +23,37 @@ function Hero() {
             <img
               className="image-one"
               src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1973&q=80"
-              alt=""
+              alt="plant image"
             />
             <img
               className="image-two"
               src="https://images.unsplash.com/photo-1459156212016-c812468e2115?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDI2fHxwbGFudHN8ZW58MHx8fHwxNjgyMjY0NTY2&ixlib=rb-4.0.3&q=85&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450"
-              alt=""
+              alt="plant image"
             />
             <img
               className="image-three"
               src="https://images.unsplash.com/photo-1680676208540-bd67f609a338?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-              alt=""
+              alt="plant image"
             />
             <img
               className="image-five"
               src="https://images.unsplash.com/photo-1545165375-1b744b9ed444?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDEwN3x8cGxhbnRzfGVufDB8fHx8MTY4MjI4MDc2OQ&ixlib=rb-4.0.3&q=85&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450"
-              alt=""
+              alt="plant image"
             />
             <img
               className="image-seven"
-              src="https://images.unsplash.com/photo-1509587584298-0f3b3a3a1797?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGV0JTIwZnJpZW5kbHklMjBwbGFudHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-              alt=""
+              src="https://images.unsplash.com/photo-1509221977468-59ebf138e25e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+              alt="plant image"
             />
             <img
               className="image-six"
               src="https://images.unsplash.com/photo-1612363228106-c94b0754e2c1?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDM1fHxpbmRvb3IlMjBwbGFudHN8ZW58MHx8fHwxNjgyNTkzNjU4&ixlib=rb-4.0.3&q=85&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450"
-              alt=""
+              alt="plant image"
             />
             <img
               className="image-four"
               src="https://images.unsplash.com/photo-1526565782131-a13074f0dbbb?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDIzfHxwb3QlMjBwbGFudHN8ZW58MHx8fHwxNjgyNjEyNzgx&ixlib=rb-4.0.3&q=85&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450"
-              alt=""
+              alt="plant image"
             />
           </div>
         </div>
