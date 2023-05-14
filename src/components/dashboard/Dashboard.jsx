93434830ba6efa1fa3230790513448308e96f324
Masa Orel <orel.masha89@gmail.com>
2023-05-14 22:16:08 +0200

    added dashboard and styles for mobile

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./dashboard.css";
+import Home from "../../pages/Home";
 
 function Dashboard({ user, isAuthenticated }) {
-  return (
-    isAuthenticated && (
-      <div className="dashboard">
-        <h1>
-          Hello to your dashboard <span>{user?.name}</span>
-        </h1>
-        <p>This is where all the info about the user will go!</p>
-      </div>
-    )
+  return isAuthenticated ? (
+    <div className="dashboard">
+      <h1>
+        Hello to your dashboard <span>{user?.name}</span>
+      </h1>
+      <p>This is where all the info about the user will go!</p>
+    </div>
+  ) : (
+    <Home />
   );
 }
 
